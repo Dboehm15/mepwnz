bfb760ff130d86cc04183009da8487777fac1a73
dboehm15 <dboehm2019@outlook.com>
2023-07-23 13:22:32 -0500

    Add meta data client

--- src/main/kotlin/com/bab/mepwnz/service/BlizzardService.kt
+++ src/main/kotlin/com/bab/mepwnz/service/BlizzardService.kt
@@ -2,6 +2,7 @@ package com.bab.mepwnz.service
 
 import com.bab.mepwnz.clients.BlizzardClient
 import com.bab.mepwnz.models.AuctionHouse
+import com.bab.mepwnz.models.RealmMetaData
 import java.util.concurrent.ExecutorService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
@@ -10,22 +11,42 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Service
 class BlizzardService (
     private val blizzardClient: BlizzardClient,
-    getGPTExecutor: ExecutorService
 ) {
     private val logger = LoggerFactory.getLogger(BlizzardService::class.java)
-    private val dispatcher = getGPTExecutor.asCoroutineDispatcher()
-    suspend fun getAuctionHouse(): AuctionHouse = coroutineScope {
+    suspend fun getAuctionHouse(realmId: Int): Mono<AuctionHouse>  {
         logger.debug("Doing the things in the service class")
-
-        val response = async(dispatcher){
-            withContext(Dispatchers.IO){
-                blizzardClient.getAuctionHouse()
-            }
+        val results = blizzardClient.getAuctionHouse(realmId)
+/*
+        results.flatMapMany { auctionHouse ->
+            Flux.fromIterable(auctionHouse.auctions)
         }
-        return@coroutineScope response.await()
+        .doOnNext { auctionItem ->
+
+        }*/
+
+        return results
+    }
+
+    suspend fun getServerId(serverName: String): Mono<Int>  {
+        return blizzardClient.getRealmMetaData()
+            .flatMapMany { metaData ->
+                Flux.fromIterable(metaData.results)
+            }
+            .flatMap { results ->
+                Flux.fromIterable(results.data.realms)
+            }
+            .filter { realm ->
+                realm.name.en_US == serverName
+            }
+            .map { realm ->
+                realm.id
+            }
+            .next()
     }
 }
